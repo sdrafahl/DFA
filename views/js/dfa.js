4edbf39e73fed171cbf77f25aab305e86542acbe
shane <sdrafahl@iastate.edu>
2017-10-12 02:09:49 -0500

    some minor modification to the plus transitions

--- views/js/dfa.js
+++ views/js/dfa.js
@@ -10,11 +10,14 @@ window.onload = function() {
   stateButton.onclick = function() {
     addState();
   }
+
+  var transButton = document.getElementById("trans");
+  transButton.onclick = function() {
+    addTransition();
+  }
 }
 
 function validate() {
-
-
    var start = document.getElementById("start").value;
    var end = document.getElementById("terminal").value;
 
@@ -38,14 +41,14 @@ function validate() {
 
 function addState() {
   tr = document.createElement("tr");
-  table = document.getElementById("table");
+  table = document.getElementById("table1");
   for(var x=0;x<localStorage.getItem("width");x++) {
     th = document.createElement("th");
     if(x == 0) {
       th.innerHTML = alphabet[localStorage.getItem("height")];
     }
     input = document.createElement("input");
-    input.id = "" + localStorage.getItem("height") + alphabet[x];
+    input.id = "" + (x + 1) + alphabet[localStorage.getItem("height")];
     th.appendChild(input);
     tr.appendChild(th);
     table.appendChild(tr);
@@ -53,4 +56,22 @@ function addState() {
   var h = localStorage.getItem("height");
   h = h * 1;
   localStorage.setItem("height", h+1);
+}
+
+function addTransition() {
+  th = document.createElement("th");
+  th.innerHTML = localStorage.getItem("height");
+  tr = document.getElementById("transitions");
+  tr.appendChild(th);
+  columns = document.getElementById("table1").childNodes[1].childNodes;
+  console.log(document.getElementById("table1").childNodes[1].childNodes);
+  for(var x=0;x<columns.length;x++) {
+    newth = document.createElement("th");
+    newInput = document.createElement("input");
+    newInput.id = localStorage.getItem("width") + alphabet[x-1];
+    newth.appendChild(newInput);
+    columns.childNodes[1].childNodes.appendChild(newth);
+  }
+  w = localStorage.getItem("width");
+  localStorage.setItem("width", w + 1);
 }
